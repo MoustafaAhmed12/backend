28c85a508d995491f1399bd251ad6fe5b4016584
MoustafaAhmed12 <ma0558317@gmail.com>
2025-08-17 19:20:26 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3000;
+
+// Send email endpoint
+app.post("/send-email", async (req, res) => {
+  try {
+    const { templateName, emails, subject } = req.body;
+    const filePath = path.join("templates", templateName);
+
+    // read html template
+    const htmlContent = fs.readFileSync(filePath, "utf-8");
+
+    // transporter
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.APP_PASSWORD,
+      },
+    });
+
+  const info = await transporter.sendMail({
+    from: `"Moustafa Ahmed" <${process.env.EMAIL}>`,
+    to: emails,
+    subject,
+    html: htmlContent,
+    attachments: [
+      {
+        filename: "email1.jpg", // الصورة للتمبلت email1
+        path: path.join("templates", "images", "1.jpeg"),
+        cid: "image1", // معرف فريد لكل صورة
+      },
+      {
+        filename: "email2.jpg",
+        path: path.join("templates", "images", "2.jpeg"),
+        cid: "image2",
+      },
+      {
+        filename: "email3.jpg",
+        path: path.join("templates", "images", "3.jpeg"),
+        cid: "image3",
+      },
+    ],
+  });
+
+
+    console.log(info.messageId);
+    res.json({ success: true, message: "Emails sent successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Error sending emails" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
