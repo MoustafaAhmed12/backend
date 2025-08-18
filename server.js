b5e27a35d952554b3d5cf37fd79279d636a8bcee
Moustafa Ahmed <89307844+MoustafaAhmed12@users.noreply.github.com>
2025-08-18 18:40:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,37 +31,16 @@ app.post("/send-email", async (req, res) => {
       },
     });
 
-    let attachments = [];
 
-if (templateName === "email1.html") {
-  attachments.push({
-    filename: "1.jpeg",
-    path: path.join("templates", "images", "1.jpeg"),
-    cid: "image1"
-  });
-} else if (templateName === "email2.html") {
-  attachments.push({
-    filename: "email2.jpeg",
-    path: path.join("templates", "images", "email2.jpeg"),
-    cid: "image2"
-  });
-} else {
-  attachments.push({
-    filename: "email3.jpeg",
-    path: path.join("templates", "images", "email3.jpeg"),
-    cid: "image3"
-  });
-}
+
 
  const info = await transporter.sendMail({
   from: `"Moustafa Ahmed" <${process.env.EMAIL}>`,
   to: emails,
   subject,
   html: htmlContent,
-  attachments,
 });
 
-    console.log(info.messageId);
     res.json({ success: true, message: "Emails sent successfully!" });
   } catch (error) {
     console.error(error);
